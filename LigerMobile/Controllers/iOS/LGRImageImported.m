3d63fa80a25a1f2ec97745291901859e92ecc9e7
John Gustafsson <john.gustafsson.liger@gmail.com>
2014-11-17 13:46:29 -0600

    Added camera option for iOS8.
    Made the image page return a UIActionController if on iOS8, with the
    question if the user wants to use the camera or the image library.

--- LigerMobile/Controllers/iOS/LGRImageImported.m
+++ LigerMobile/Controllers/iOS/LGRImageImported.m
@@ -18,10 +18,46 @@
 
 + (UIViewController*)controllerForImportedPage:(NSString*)page title:(NSString*)title args:(NSDictionary*)args options:(NSDictionary*)options parent:(LGRViewController*)parent
 {
-	UIImagePickerController *picker = [[UIImagePickerController alloc] init];
-	picker.delegate = parent;
-	
-	return picker;	
+	if (![self isAlertControllerAvailable]) {
+		UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+		picker.delegate = parent;
+
+		return picker;
+	}
+
+	return [self alertController:parent];
+}
+
++ (BOOL)isAlertControllerAvailable
+{
+	return [UIAlertController class];
+}
+
++ (UIAlertController*)alertController:(LGRViewController *)parent
+{
+	UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+
+	if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+		[alertController addAction:[UIAlertAction actionWithTitle:@"Camera" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+			UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+			picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+			picker.delegate = parent;
+			[parent presentViewController:picker animated:YES completion:nil];
+		}]];
+	}
+
+	[alertController addAction:[UIAlertAction actionWithTitle:@"Photo Library" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+		UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+		picker.delegate = parent;
+		[parent presentViewController:picker animated:YES completion:nil];
+	}]];
+
+	[alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+
+	}]];
+
+
+	return alertController;
 }
 
 @end
